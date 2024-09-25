3b70823adde2d544e5f0026a567501a8cb28bbe4
Cynox <Cynox336@gmail.com>
2024-09-25 12:30:58 +0200

    Update vercel

--- scripts.js
+++ scripts.js
@@ -1,4 +1,19 @@
 import { data } from './data.js';
+import { sql } from "@vercel/postgres";
+
+export default async function Cart({ params }) {
+  const { rows } = await sql`SELECT * from CARTS where user_id=${params.user}`;
+
+    return (
+    <div>
+    {rows.map((row) => (
+        <div key={row.id}>
+        {row.id} - {row.quantity}
+        </div>
+    ))}
+    </div>
+    );
+}
 
 window.onload = function() {
     loadData();
